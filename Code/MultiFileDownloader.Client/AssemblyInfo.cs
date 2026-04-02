53c7335c1d38e7eb27acbc315f9764912c5f7028
Sad Boy <nh419630@gmail.com>
2026-04-02 20:39:31 +0700

    Update AssemblyInfo.cs

--- Code/MultiFileDownloader.Client/AssemblyInfo.cs
+++ Code/MultiFileDownloader.Client/AssemblyInfo.cs
@@ -1,16 +1,10 @@
 using System.Windows;
 
-// Khai báo thông tin về Resource (style, theme) của ứng dụng WPF
 [assembly: ThemeInfo(
-
-    // Vị trí chứa ResourceDictionary theo từng theme (Light, Dark...)
-    // None = không dùng theme riêng
-    // Nếu không tìm thấy resource ở page hoặc app thì cũng không tìm theo theme
-    ResourceDictionaryLocation.None,
-
-    // Vị trí chứa ResourceDictionary chung (generic)
-    // SourceAssembly = nằm trong chính project hiện tại
-    // Nếu không tìm thấy resource ở page, app hoặc theme thì sẽ tìm ở đây
-    ResourceDictionaryLocation.SourceAssembly
-
+    ResourceDictionaryLocation.None,            //where theme specific resource dictionaries are located
+                                                //(used if a resource is not found in the page,
+                                                // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly   //where the generic resource dictionary is located
+                                                //(used if a resource is not found in the page,
+                                                // app, or any theme specific resource dictionaries)
 )]

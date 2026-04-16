2e4972a8cc0193cd3f6adf42ff7a4b1961186d39
nhnnguyen712 <nhnnguyen71206@gmail.com>
2026-04-17 00:40:22 +0700

    Enable multi-select and drag-drop

--- Code/MultiFileDownloader.Client/MainWindow.xaml.cs
+++ Code/MultiFileDownloader.Client/MainWindow.xaml.cs
@@ -5,6 +5,8 @@ using System.IO;
 using System.Net.Sockets;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace MultiFileDownloader.Client
 {
@@ -18,6 +20,10 @@ namespace MultiFileDownloader.Client
         SemaphoreSlim semaphore = new SemaphoreSlim(3);
 
         int activeDownloads = 0;
+
+        // Cho Ctrl/Shift Click functionality
+        private ServerFileItem lastSelectedItem = null;
+        private ServerFileItem itemBeingClicked = null;
 
         public MainWindow()
         {
@@ -55,6 +61,181 @@ namespace MultiFileDownloader.Client
                 {
                     FileName = f
                 });
+            }
+        }
+
+        // ================= MULTIPLE SELECTION =================
+
+        private void lbServerFiles_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var item = GetItemAtPoint(lbServerFiles, e.GetPosition(lbServerFiles));
+
+            if (item != null)
+            {
+                itemBeingClicked = item; // Track item được click
+
+                // Kiểm tra nếu click vào checkbox, để nó handle bình thường
+                if (IsClickOnCheckBox(e, item))
+                {
+                    lastSelectedItem = item;
+                    return; // Không handle, để checkbox tự xử lý
+                }
+
+                if (Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl))
+                {
+                    // Ctrl+Click: Toggle selection
+                    item.IsSelected = !item.IsSelected;
+                    lastSelectedItem = item;
+                    e.Handled = true;
+                }
+                else if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
+                {
+                    // Shift+Click: Range selection
+                    if (lastSelectedItem != null)
+                    {
+                        SelectRange(lastSelectedItem, item);
+                    }
+                    else
+                    {
+                        item.IsSelected = true;
+                        lastSelectedItem = item;
+                    }
+                    e.Handled = true;
+                }
+                else
+                {
+                    // Normal click vào text (không phải checkbox): Toggle selection
+                    item.IsSelected = !item.IsSelected;
+                    lastSelectedItem = item;
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is CheckBox checkbox && checkbox.DataContext is ServerFileItem item)
+            {
+                lastSelectedItem = item;
+            }
+        }
+
+        private void lbServerFiles_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Handled by PreviewMouseLeftButtonDown
+        }
+
+        private void SelectRange(ServerFileItem from, ServerFileItem to)
+        {
+            int indexFrom = lbServerFiles.Items.IndexOf(from);
+            int indexTo = lbServerFiles.Items.IndexOf(to);
+
+            if (indexFrom > indexTo)
+            {
+                (indexFrom, indexTo) = (indexTo, indexFrom);
+            }
+
+            for (int i = 0; i < lbServerFiles.Items.Count; i++)
+            {
+                if (lbServerFiles.Items[i] is ServerFileItem item)
+                {
+                    item.IsSelected = (i >= indexFrom && i <= indexTo);
+                }
+            }
+        }
+
+        private ServerFileItem GetItemAtPoint(ListBox listBox, Point point)
+        {
+            var result = VisualTreeHelper.HitTest(listBox, point);
+            var visual = result?.VisualHit;
+
+            while (visual != null)
+            {
+                if (VisualTreeHelper.GetParent(visual) is ListBoxItem itemContainer)
+                {
+                    return listBox.ItemContainerGenerator.ItemFromContainer(itemContainer) as ServerFileItem;
+                }
+                visual = VisualTreeHelper.GetParent(visual) as Visual;
+            }
+
+            return null;
+        }
+
+        private bool IsClickOnCheckBox(MouseButtonEventArgs e, ServerFileItem item)
+        {
+            var hitTest = VisualTreeHelper.HitTest(lbServerFiles, e.GetPosition(lbServerFiles));
+            var visual = hitTest?.VisualHit;
+
+            while (visual != null)
+            {
+                if (visual is CheckBox)
+                    return true;
+
+                visual = VisualTreeHelper.GetParent(visual) as Visual;
+            }
+
+            return false;
+        }
+
+        // ================= DRAG & DROP =================
+
+        private void lbServerFiles_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                var item = GetItemAtPoint(lbServerFiles, e.GetPosition(lbServerFiles));
+
+                if (item != null && itemBeingClicked == item)
+                {
+                    // Khi detect drag bắt đầu, nếu item không được select, tự động select nó
+                    // (Để drag & drop hoạt động, ngay cả khi user vừa toggle untick)
+                    if (!item.IsSelected)
+                    {
+                        item.IsSelected = true;
+                    }
+
+                    // Lấy tất cả selected items
+                    var selectedFiles = lbServerFiles.Items
+                        .OfType<ServerFileItem>()
+                        .Where(x => x.IsSelected)
+                        .Select(x => x.FileName)
+                        .ToList();
+
+                    if (selectedFiles.Count > 0)
+                    {
+                        var dataObject = new DataObject();
+                        dataObject.SetData("ServerFiles", selectedFiles);
+
+                        DragDrop.DoDragDrop(lbServerFiles, dataObject, DragDropEffects.Copy);
+
+                        itemBeingClicked = null; // Reset
+                    }
+                }
+            }
+        }
+
+        private void lvDownloads_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent("ServerFiles"))
+            {
+                e.Effects = DragDropEffects.Copy;
+                e.Handled = true;
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
+
+        private void lvDownloads_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetData("ServerFiles") is List<string> files)
+            {
+                foreach (var fileName in files)
+                {
+                    _ = StartNewDownload(fileName);
+                }
+                e.Handled = true;
             }
         }
 
@@ -188,12 +369,11 @@ namespace MultiFileDownloader.Client
 
             Process.Start("explorer.exe", path);
         }
-        private void lbServerFiles_SelectionChanged(object sender, SelectionChangedEventArgs e)
+
+        private void ClearAll()
         {
-            foreach (ServerFileItem item in e.AddedItems)
-            {
-                item.IsSelected = true;
-            }
+            foreach (ServerFileItem item in lbServerFiles.Items)
+                item.IsSelected = false;
         }
     }
 }
